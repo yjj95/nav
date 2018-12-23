13a0cd1bfec33e883502d97d8e568c67a6925110
jia <15901226963@163.com>
2018-12-23 22:03:54 +0800

    添加一些键，封装

--- js/main.js
+++ js/main.js
@@ -3,64 +3,19 @@ var data=init()
 var arr=data.arr
 var hash=data.hash
 //2.生成键盘
+generateKeyboard(arr,hash)
 //3.监听事件
-
- for(var index=0;index<arr.length;index++){
-     div=tag("div")
-     var row=arr[index]//里层数组
-     for(var index2=0;index2<row.length;index2++){
-         kbd=tag("kbd");
-         kbd.textContent=row[index2]
-         div.appendChild(kbd)
-         
-         //favicon
-         img=tag("img")
-         kbd.appendChild(img);
-         //添加src
-         //存在情况一定要写else也要写
-         //如果这个icon不存在就要设置一个肯定存在的
-         //图片请求出错
-         var key=row[index2]
-         createImageSrc(hash[key])
-         
-
-         //点击编辑按钮编辑网址
-         var btn=tag("button")
-         btn.textContent="编辑"//
-         btn.id=row[index2]
-         kbd.appendChild(btn)
-         //点击编辑
-         btn.onclick=function(e){
-             k=e.target.id
-             x=prompt("给我一个网址")
-             //拿到网址放到这个键对应的hash里
-             hash[k]=x;//现在的hash发生变化了
-             //此时icon也要变,此时icon肯定存在
-             //如果这不写改src，那么刷新后才会改
-             //?编辑不能立马改变
-             //x就是新网址，src里直接写x
-             createImageSrc(x)
-             //把整个hash存起来不是仅存变化
-             localStorage.setItem("xh",JSON.stringify(hash))
-             
-         }
-     }
-     main.appendChild(div);
- }
- 
- 
  document.onkeypress=function(e){
      var key=e.key//新开一个窗口打开
      window.open("http://"+hash[key],"_blank")
  }
- //生成html结构、内容，添加事件，编辑hash，使用localstorage,favicon,图片加载失败两种原因,封装,css,对比，熟练
- //...背景，制作图片，使用工具查阴影
+ 
  //下面是工具函数
  function init(){
     var arr={
-            '0': {0:'q',1:'w',2:'e',3:'r',4:'t',5:'y',6:'u',7:'i',8:'o',9:'p',length:10},
-             '1': {0:'a',1:'s',2:'d',3:'f',4:'g',5:'h',6:'j',7:'k',8:'l',length:9},
-             '2': {0:'z',1:'x',2:'c',3:'v',4:'b',5:'n',6:'m',length:7},
+            '0': {0:'tab',1:'q',2:'w',3:'e',4:'r',5:'t',6:'y',7:'u',8:'i',9:'o',10:'p',11:'{',12:'}',13:'|',length:14},
+             '1': {0:'caps lock',1:'a',2:'s',3:'d',4:'f',5:'g',6:'h',7:'j',8:'k',9:'l',10:':',11:'"',12:'enter',length:13},
+             '2': {0:'shift',1:'z',2:'x',3:'c',4:'v',5:'b',6:'n',7:'m',8:'<',9:'>',10:'?',11:'shift',length:12},
              'length': 3
          };
     var hash = {'q': 'qq.com', 'w': 'weibo.com', 'e': 'ele.me', 'r': 'renren.com', 't': 'tianya.com', 'y': 'youtube.com', 'u': 'uc.com' , 'i': 'iqiyi.com', 'o': 'opera.com', 'p': undefined, 'a': 'acfun.tv', 's': 'sohu.com', 'z': 'zhihu.com', 'm': 'www.mcdonalds.com.cn'
@@ -73,17 +28,38 @@ var hash=data.hash
     return {arr:arr,hash:hash}
 }
 function createImageSrc(domain){
+    //存在情况一定要写else也要写
+    //如果这个icon不存在就要设置一个肯定存在的
+    //图片请求出错
      if(domain){
          img.src="http://"+domain+"/favicon.ico"
      }else{
          img.src="//i.loli.net/2017/11/10/5a05afbc5e183.png"//.
      }
-    //  img.onerror=function(e){
-    //      e.target.src="//i.loli.net/2017/11/10/5a05afbc5e183.png"//x
-    //  }
+      img.onerror=function(e){
+          e.target.src="//i.loli.net/2017/11/10/5a05afbc5e183.png"//x
+      }
  }
- function createButton(){
-
+ function createButton(id){
+    //点击编辑按钮编辑网址
+    var btn=tag("button")
+    btn.textContent="编辑"//
+    btn.id=id
+    //点击编辑
+    btn.onclick=function(e){
+        k=e.target.id
+        x=prompt("给我一个网址")
+        //拿到网址放到这个键对应的hash里
+        hash[k]=x;//现在的hash发生变化了
+        //此时icon也要变,此时icon肯定存在
+        //如果这不写改src，那么刷新后才会改
+        //?编辑不能立马改变
+        //x就是新网址，src里直接写x
+        createImageSrc(x)
+        //把整个hash存起来不是仅存变化
+        localStorage.setItem("xh",JSON.stringify(hash))
+    }
+    return btn
  }
  function createSpan(){
 
@@ -94,6 +70,43 @@ function createImageSrc(domain){
  function getFormLocalstorage(key){
     return JSON.parse(localStorage.getItem(key)||"null")
  }
- function generateKeyboard(){
-
+ function generateKeyboard(word,domain){
+    for(var index=0;index<word.length;index++){
+        div=tag("div")
+        var row=word[index]//里层数组
+        for(var index2=0;index2<row.length;index2++){
+            kbd=tag("kbd");
+            kbd.textContent=row[index2]
+            if(row[index2].length>1){
+                kbd.classList.add('special')
+            }
+            switch (row[index2]) {
+                case 'tab':
+                kbd.classList.add(row[index2])
+                    break;
+                case 'caps lock':
+                kbd.className='caps-lock'
+                    break;
+                case 'enter':
+                kbd.className=row[index2]
+                    break;
+                case 'shift':
+                kbd.className=row[index2]
+                    break;
+                default:
+                    break;
+            }
+            div.appendChild(kbd)
+            //favicon
+            img=tag("img")
+            kbd.appendChild(img);
+            //a-z
+            var key=row[index2]
+            //添加src
+            createImageSrc(domain[key])
+            var btn=createButton(key)
+            kbd.appendChild(btn)
+        }
+        main.appendChild(div);
+    }
  }
